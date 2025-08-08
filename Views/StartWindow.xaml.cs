d4cc928656cb0560029badf64e37139daf7c5d16
Bosguega <bosguega@gmail.com>
2025-08-08 15:22:46 -0300

    Adicionar arquivos de projeto.

---
+++ Views/StartWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using DevToolVault.Views;
+
+namespace DevToolVault.Views
+{
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+
+            // Carregar a página inicial (ex: Estrutura)
+            MainFrame.Navigate(new EstruturaPage());
+        }
+
+        private void MenuItem_Estrutura_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new EstruturaPage());
+        }
+
+        private void MenuItem_ExportarCodigo_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new ExportarCodigoPage());
+        }
+
+        private void MenuItem_Sair_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MenuItem_Sobre_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("DevToolVault - Coleção de utilitários para desenvolvedores.\n\nVersão 1.0", "Sobre");
+        }
+    }
+}
